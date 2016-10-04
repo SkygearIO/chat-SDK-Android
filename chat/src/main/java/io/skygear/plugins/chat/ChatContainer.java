da0b3bcd9fdd39ef84ac67e9738e0b8fc5634556
Johnny Kao <kaojohnny@gmail.com>
2016-10-04 17:17:40 +0800

    Support getMessages

--- chat/src/main/java/io/skygear/plugins/chat/ChatContainer.java
+++ chat/src/main/java/io/skygear/plugins/chat/ChatContainer.java
@@ -1,16 +1,21 @@
 package io.skygear.plugins.chat;
 
 
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import io.skygear.skygear.Container;
 import io.skygear.skygear.Database;
+import io.skygear.skygear.LambdaResponseHandler;
 import io.skygear.skygear.Query;
 import io.skygear.skygear.Record;
 import io.skygear.skygear.RecordQueryResponseHandler;
@@ -119,6 +124,73 @@ public final class ChatContainer {
                 }
             }
         });
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    public void getMessages(final String conversationId,
+                            final int limit,
+                            @Nullable final Date before,
+                            final GetCallback<Message> callback) {
+        final int LIMIT = 50; // default value
+
+        if (conversationId != null && conversationId.length() != 0) {
+            int limitCount = limit;
+            String beforeTimeISO8601 = DateUtil.toISO8601(before != null ? before : new Date());
+
+            if (limitCount <= 0) {
+                limitCount = LIMIT;
+            }
+
+            Object[] args = new Object[]{conversationId, limitCount, beforeTimeISO8601};
+            container.callLambdaFunction("chat:get_messages", args, new LambdaResponseHandler() {
+                @Override
+                public void onLambdaSuccess(JSONObject result) {
+                    List<Message> messages = buildMessages(result.optJSONArray("results"));
+                    callback.done(messages, null);
+                }
+
+                @Override
+                public void onLambdaFail(String reason) {
+                    callback.done(null, reason);
+                }
+            });
+        }
+    }
+
+    private List<Message> buildMessages(final JSONArray results) {
+        List<Message> messages = null;
+
+        if (results != null) {
+            messages = new ArrayList<>(results.length());
+
+            for (int i = 0; i < results.length(); i++) {
+                try {
+                    JSONObject object = results.getJSONObject(i);
+                    Message message = buildMessage(object);
+                    if (message != null) {
+                        messages.add(message);
+                    }
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, "buildMessages: " + e.getMessage());
+                }
+            }
+        }
+
+        return messages;
+    }
+
+    private Message buildMessage(final JSONObject object) {
+        Message message = null;
+
+        try {
+            Record record = Record.fromJson(object);
+            message = new Message(record);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "buildMessage: " + e.getMessage());
+        }
+
+        return message;
     }
 
     //----------------------------------------------------------------------------------------------
