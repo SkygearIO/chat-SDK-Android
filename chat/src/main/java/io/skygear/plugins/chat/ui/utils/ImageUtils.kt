4e78ac578c933c782933ab2e804ad9d339714dd4
Carmen <carmen@oursky.com>
2017-10-12 17:53:36 +0800

    Handle image orientation

--- chat/src/main/java/io/skygear/plugins/chat/ui/utils/ImageUtils.kt
+++ chat/src/main/java/io/skygear/plugins/chat/ui/utils/ImageUtils.kt
@@ -3,6 +3,7 @@ package io.skygear.plugins.chat.ui.utils
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Environment
 import java.io.ByteArrayOutputStream
@@ -10,6 +11,9 @@ import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import android.media.ExifInterface
+import android.R.attr.path
+import java.io.FileOutputStream
 
 
 /**
@@ -36,25 +40,48 @@ fun getResizedBitmap(context: Context, uri: Uri): ImageData? {
         return null
     }
 
+    // get orientation exif
+    input = context.getContentResolver().openInputStream(uri)
+    val file = File.createTempFile("image_tmp", ".jpg", context.getCacheDir())
+    val fos = FileOutputStream(file)
+
+    var len: Int
+    val buffer = ByteArray(1024)
+
+    do {
+        len = input.read(buffer, 0, 1024)
+        if (len == -1)
+            break
+        fos.write(buffer, 0, len)
+    } while (true)
+    input.close()
+    fos.close()
+
+    val ef = ExifInterface(file.toString())
+    val orientation = ef.getAttributeInt(ExifInterface.TAG_ORIENTATION, 99)
+
     val originalSize = if (onlyBoundsOptions.outHeight > onlyBoundsOptions.outWidth)
         onlyBoundsOptions.outHeight else onlyBoundsOptions.outWidth
 
     val imgRatio = if (originalSize > IMAGE_SIZE) originalSize / IMAGE_SIZE else 1.0
-    val bitmap = getBitmap(context, uri, imgRatio)
+    val bitmap = getBitmap(context, uri, imgRatio, orientation)
 
     val thumbRatio = if (originalSize > THUMBNAIL_SIZE) originalSize / THUMBNAIL_SIZE else 1.0
-    val thumbBitmap = getBitmap(context, uri, thumbRatio)
+    val thumbBitmap = getBitmap(context, uri, thumbRatio, orientation)
 
     return ImageData(thumbBitmap, bitmap)
 }
 
-fun getBitmap(context: Context, uri: Uri, ratio: Double) : Bitmap {
+fun getBitmap(context: Context, uri: Uri, ratio: Double, orientation: Int) : Bitmap {
     val bitmapOptions = BitmapFactory.Options()
     bitmapOptions.inSampleSize = getPowerOfTwoForSampleRatio(ratio)
     bitmapOptions.inPreferredConfig = Bitmap.Config.ARGB_8888
 
     val input = context.getContentResolver().openInputStream(uri)
-    val bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions)
+    var bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions)
+
+    bitmap = rotateBitmap(bitmap, orientation)
+
     input.close()
 
     return bitmap
@@ -64,6 +91,39 @@ fun bitmapToByteArray(bmp: Bitmap): ByteArray? {
     val stream = ByteArrayOutputStream()
     bmp.compress(Bitmap.CompressFormat.JPEG, 70, stream)
     return stream.toByteArray()
+}
+
+fun rotateBitmap(bitmap: Bitmap, orientation: Int): Bitmap? {
+    val matrix = Matrix()
+    when (orientation) {
+        ExifInterface.ORIENTATION_NORMAL -> return bitmap
+        ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> matrix.setScale(-1f, 1f)
+        ExifInterface.ORIENTATION_ROTATE_180 -> matrix.setRotate(180f)
+        ExifInterface.ORIENTATION_FLIP_VERTICAL -> {
+            matrix.setRotate(180f)
+            matrix.postScale(-1f, 1f)
+        }
+        ExifInterface.ORIENTATION_TRANSPOSE -> {
+            matrix.setRotate(90f)
+            matrix.postScale(-1f, 1f)
+        }
+        ExifInterface.ORIENTATION_ROTATE_90 -> matrix.setRotate(90f)
+        ExifInterface.ORIENTATION_TRANSVERSE -> {
+            matrix.setRotate(-90f)
+            matrix.postScale(-1f, 1f)
+        }
+        ExifInterface.ORIENTATION_ROTATE_270 -> matrix.setRotate(-90f)
+        else -> return bitmap
+    }
+    try {
+        val bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        bitmap.recycle()
+        return bmRotated
+    } catch (e: OutOfMemoryError) {
+        e.printStackTrace()
+        return null
+    }
+
 }
 
 @Throws(IOException::class)
