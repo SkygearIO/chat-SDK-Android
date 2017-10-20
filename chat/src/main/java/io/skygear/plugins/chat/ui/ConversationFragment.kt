d98f2f9d90e0b3d697c09a0fffb9f6c028a2ebdb
Carmen <carmen@oursky.com>
2017-10-20 14:22:57 +0800

    Set recycler view to disable isAutoMeasureEnabled, workaround for scroll view move unexpectedly when update issue. #65
    `isAutoMeasureEnabled` is used for supporting RecyclerView to measure is size when items changed. And this feature cause the RecycleView scroll unexpectedly when update, and it should be bugs of RecycleView.
    In our case, the message list view is match_parent. It is safe for us to disable it.

--- chat/src/main/java/io/skygear/plugins/chat/ui/ConversationFragment.kt
+++ chat/src/main/java/io/skygear/plugins/chat/ui/ConversationFragment.kt
@@ -128,6 +128,7 @@ class ConversationFragment : Fragment(),
             this.messagesListViewReachBottomListener = MessagesListViewReachBottomListener(
                     this.messagesListView?.layoutManager as LinearLayoutManager
             )
+            (this.messagesListView?.layoutManager as LinearLayoutManager).isAutoMeasureEnabled = false
             this.messagesListView?.addOnScrollListener(this.messagesListViewReachBottomListener)
         }
 
