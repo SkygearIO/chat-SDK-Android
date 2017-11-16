fda7930d66ce757f6f753d346932df5966447af7
Carmen <carmen@oursky.com>
2017-11-16 11:09:36 +0800

    Update buddy script to upload build in master branch

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -2,12 +2,14 @@
 
 echo "Uploading apk to HockeyApp..."
 
-if [[ "$BUDDYBUILD_VARIANTS" =~ "release" ]]; then
+
+if [[ "$BUDDYBUILD_BRANCH" == "build-test" ]]; then
+  APK_PATH=($(find . -name *-release.apk))
   curl \
     -F "release_type=2" \
     -F "status=2" \
     -F "notify=0" \
-    -F "ipa=@$BUDDYBUILD_WORKSPACE/chat_example/build/outputs/apk/chat_example-release.apk" \
+    -F "ipa=@$APK_PATH" \
     -H "X-HockeyAppToken: $HOCKEYAPP_APPTOKEN" \
     https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APPID/app_versions/upload
   echo "Finished uploading apk to HockeyApp."
