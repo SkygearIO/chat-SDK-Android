c9b10137c25b729f699114ef48a3181cb764ef0b
Steven Chan <stevenchan@oursky.com>
2017-11-23 20:34:22 +0800

    Implement realm store

--- chat/src/main/java/io/skygear/plugins/chat/RealmStore.java
+++ chat/src/main/java/io/skygear/plugins/chat/RealmStore.java
@@ -34,6 +34,68 @@ class RealmStore {
         return Realm.getInstance(configBuilder.build());
     }
 
+    Message[] getMessages(RealmQuery<MessageCacheObject> query,
+                          int limit,
+                          String order) {
+        OrderedRealmCollectionSnapshot<MessageCacheObject> results = query.findAllSorted(order, Sort.DESCENDING).createSnapshot();
+        int size = results.size();
+        if (limit == -1 || limit > size) {
+            limit = size;
+        }
+
+        List<Message> messages = new ArrayList<>(limit);
+        for (int i = 0; i < limit; i++) {
+            MessageCacheObject cacheObject = results.get(i);
+            messages.add(cacheObject.toMessage());
+        }
+
+        Message[] messageArray = new Message[messages.size()];
+        return messages.toArray(messageArray);
+    }
+
+    Message getMessageWithID(String messageID) {
+        MessageCacheObject cacheObject = getRealm().where(MessageCacheObject.class).equalTo("recordID", messageID).findFirst();
+        if (cacheObject == null) {
+            return null;
+        }
+
+        return cacheObject.toMessage();
+    }
+
+    void setMessages(Message[] messages) {
+        Realm realm = getRealm();
+
+        realm.beginTransaction();
+
+        List<MessageCacheObject> cacheObjects = new ArrayList<>(messages.length);
+        for (Message message : messages) {
+            MessageCacheObject cacheObject = new MessageCacheObject(message);
+            cacheObjects.add(cacheObject);
+        }
+
+        realm.insertOrUpdate(cacheObjects);
+
+        realm.commitTransaction();
+    }
+
+    void deleteMessages(Message[] messages) {
+        Realm realm = getRealm();
+
+        String[] messageIDs = new String[messages.length];
+        for (int i = 0; i < messages.length; i++) {
+            messageIDs[i] = messages[i].getId();
+        }
+
+        RealmResults<MessageCacheObject> cacheObjects =
+                realm.where(MessageCacheObject.class).in("recordID", messageIDs).findAll();
+
+        realm.beginTransaction();
+
+        cacheObjects.deleteAllFromRealm();
+
+        realm.commitTransaction();
+    }
+
     @RealmModule(library = true, allClasses = true)
     private static class SkygearChatModule {
 
