ff95df90951794df5ca408d574692125f674edd7
Ho Wa <howa@oursky.com>
2017-12-02 22:19:25 +0800

    Separate UI Kit Classes from chat project
    1. Create chat_ui project
    2. Move io.skygear.plugins.chat.ui classes into chat_ui project
    3. Add bintray script
    4. Update script/deploy.sh
    5. Add :chat_ui:build into .travis.yml
    
    Connects #81

--- chat_ui/src/main/kotlin/io.skygear.plugins.chat.ui/holder/SenderAvatarMessageView.kt
+++ chat_ui/src/main/kotlin/io.skygear.plugins.chat.ui/holder/SenderAvatarMessageView.kt
@@ -2,6 +2,7 @@ package io.skygear.plugins.chat.ui.holder
 
 import android.view.View
 import android.widget.LinearLayout
+import io.skygear.plugins.chat.ui.R
 import io.skygear.plugins.chat.ui.model.Message
 
 
@@ -10,7 +11,7 @@ class SenderAvatarMessageView(itemView: View){
     var avatarView: LinearLayout? = null
 
     init {
-        avatarView = itemView.findViewById<LinearLayout>(io.skygear.plugins.chat.R.id.userAvatar)
+        avatarView = itemView.findViewById<LinearLayout>(R.id.userAvatar)
     }
 
     fun onBind(message: Message) {
