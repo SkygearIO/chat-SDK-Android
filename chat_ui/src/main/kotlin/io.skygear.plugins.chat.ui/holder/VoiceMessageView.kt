ff28e0e929591437f4eb843cbbe194df1907c6be
Ho Wa <howa@oursky.com>
2017-12-02 22:19:25 +0800

    Separate UI Kit Classes from chat project
    1. Create chat_ui project
    2. Move io.skygear.plugins.chat.ui classes into chat_ui project
    3. Add bintray script
    4. Update script/deploy.sh
    5. Add :chat_ui:build into .travis.yml
    6. Add realm and findbugs dependencies
    
    Connects #81

--- chat_ui/src/main/kotlin/io.skygear.plugins.chat.ui/holder/VoiceMessageView.kt
+++ chat_ui/src/main/kotlin/io.skygear.plugins.chat.ui/holder/VoiceMessageView.kt
@@ -4,6 +4,7 @@ import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import io.skygear.plugins.chat.ui.R
 import io.skygear.plugins.chat.ui.model.VoiceMessage
 
 
@@ -14,9 +15,9 @@ class VoiceMessageView(itemView: View){
     var timeTextView: TextView? = null
 
     init {
-        this.actionButton = itemView.findViewById<ImageView>(io.skygear.plugins.chat.R.id.action_button)
-        this.durationTextView = itemView.findViewById<TextView>(io.skygear.plugins.chat.R.id.duration)
-        this.timeTextView = itemView.findViewById<TextView>(io.skygear.plugins.chat.R.id.messageTime)
+        this.actionButton = itemView.findViewById<ImageView>(R.id.action_button)
+        this.durationTextView = itemView.findViewById<TextView>(R.id.duration)
+        this.timeTextView = itemView.findViewById<TextView>(R.id.messageTime)
     }
 
     fun onBind(message: VoiceMessage) {
@@ -27,8 +28,8 @@ class VoiceMessageView(itemView: View){
 
             this@VoiceMessageView.timeTextView?.setTextColor(ContextCompat.getColor(actionButton?.context, io.skygear.chatkit.R.color.dark_gray))
             val actionButtonIcon = when (msg.state) {
-                VoiceMessage.State.PLAYING -> io.skygear.plugins.chat.R.drawable.ic_pause_white
-                else -> io.skygear.plugins.chat.R.drawable.ic_play_white
+                VoiceMessage.State.PLAYING -> R.drawable.ic_pause_white
+                else -> R.drawable.ic_play_white
             }
             this@VoiceMessageView.actionButton?.setImageResource(actionButtonIcon)
         }
