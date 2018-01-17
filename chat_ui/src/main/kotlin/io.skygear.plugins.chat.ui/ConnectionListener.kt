f489e721d260f35d4a5f0f4051baf11a35a14248
Ho Wa <howa@oursky.com>
2018-01-17 14:40:58 +0800

    Minor Fix
    1. ConnectionListener as Serializable
    2. Add example code in ConversationsActivity

--- chat_ui/src/main/kotlin/io.skygear.plugins.chat.ui/ConnectionListener.kt
+++ chat_ui/src/main/kotlin/io.skygear.plugins.chat.ui/ConnectionListener.kt
@@ -1,6 +1,8 @@
 package io.skygear.plugins.chat.ui
 
-open interface ConnectionListener {
+import java.io.Serializable
+
+open interface ConnectionListener: Serializable {
     fun onOpen(fragment: ConversationFragment)
     fun onClose(fragment: ConversationFragment)
     fun onError(fragment: ConversationFragment, e: Exception?)
