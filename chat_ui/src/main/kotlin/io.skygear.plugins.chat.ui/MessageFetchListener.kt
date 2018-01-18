9c70da1f4bbae0adad566c7678c980484126c3e7
Ho Wa <howa@oursky.com>
2018-01-18 16:54:39 +0800

    Update listeners
    - Add MessageSentListener and MessageFetchListener in ConversationsActivity
    - Make MessageSentListener and  MessageFetchListener serialisable
    
    Connects #103

--- chat_ui/src/main/kotlin/io.skygear.plugins.chat.ui/MessageFetchListener.kt
+++ chat_ui/src/main/kotlin/io.skygear.plugins.chat.ui/MessageFetchListener.kt
@@ -2,8 +2,9 @@ package io.skygear.plugins.chat.ui
 
 import io.skygear.plugins.chat.Message
 import io.skygear.skygear.Error
+import java.io.Serializable
 
-open interface MessageFetchListener {
+open interface MessageFetchListener: Serializable{
     fun onBeforeMessageFetch(fragment: ConversationFragment)
     fun onMessageFetchFailed(fragment: ConversationFragment, error: Error)
     fun onMessageFetchSuccess(fragment: ConversationFragment, messages: List<Message>, isCached: Boolean)
