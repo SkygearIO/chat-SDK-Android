7cae21195faf9beda17265b08baa69babb66b66c
camerash <esmond9981127@gmail.com>
2018-04-29 16:28:00 +0800

    Adjusted check login logic

--- chat_example/src/main/java/io/skygear/chatexample/apitask/CreateGroupConversation.kt
+++ chat_example/src/main/java/io/skygear/chatexample/apitask/CreateGroupConversation.kt
@@ -3,7 +3,6 @@ package io.skygear.chatexample.apitask
 import android.app.Activity
 import android.content.Context
 import android.view.View
-import android.widget.Toast
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import io.skygear.chatexample.ApiTask
@@ -30,12 +29,6 @@ class CreateGroupConversation: ApiTestModule {
     }
 
     override fun onApiTest(activity: Activity, skygear: Container, chatContainer: ChatContainer, task: ApiTask, view: View) {
-        if (!Utils.isLoggedIn(activity)) {
-            Toast.makeText(activity, "Please login first!", Toast.LENGTH_SHORT).show()
-            activity.finish()
-            return
-        }
-
         val userIDs = Gson().fromJson<MutableSet<String>>(task.params["userIDs"])
         val timestamp = SimpleDateFormat("yyyy-MM-dd_HH:mm", Locale.getDefault()).format(Calendar.getInstance().time)
         Log.i(TAG, "Creating conversation...")
