ce26b40b518bd4c2f7735b1045b895593f94eaf4
camerash <esmond9981127@gmail.com>
2018-04-29 16:27:46 +0800

    Finished FetchUserConversation module

---
+++ chat_example/src/main/java/io/skygear/chatexample/apitask/FetchUserConversations.kt
@@ -0,0 +1,59 @@
+package io.skygear.chatexample.apitask
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import io.skygear.chatexample.ApiTask
+import io.skygear.chatexample.logger.Log
+import io.skygear.plugins.chat.ChatContainer
+import io.skygear.plugins.chat.Conversation
+import io.skygear.plugins.chat.GetCallback
+import io.skygear.skygear.Container
+import io.skygear.skygear.Error
+
+/**
+ * Created by camerash on 4/29/18.
+ * Fetch user conversations API Test Module
+ */
+class FetchUserConversations: ApiTestModule {
+
+    var listView: ListView? = null
+    val TAG = javaClass.simpleName
+
+    override fun onLoadCustomView(context: Context): View? {
+        listView = ListView(context)
+        return listView
+    }
+
+    override fun onApiTest(activity: Activity, skygear: Container, chatContainer: ChatContainer, task: ApiTask, view: View) {
+
+        Log.i(TAG, "Fetching conversations...")
+        chatContainer.getConversations(object: GetCallback<List<Conversation>>{
+            override fun onSucc(obj: List<Conversation>?) {
+                if(obj == null) {
+                    Log.i(TAG, "Fetch conversations failed with reason: Conversation null")
+                    return
+                }
+
+                val idList = obj.map { it.id }.toMutableList()
+                val adapter = ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, idList)
+                listView?.adapter = adapter
+                listView?.setOnItemLongClickListener { _, _, i, _ ->
+                    Utils.copyToClipboard(activity, TAG, idList[i])
+                    Toast.makeText(activity, "Conversation ID copied", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                Log.i(TAG, "Fetch conversation successful")
+                Log.i(TAG, "Long click the items above to copy the respective conversation ID")
+            }
+
+            override fun onFail(error: Error) {
+                Log.i(TAG, "Fetch conversations failed with reason: ${error.detailMessage}")
+            }
+        })
+    }
+}
