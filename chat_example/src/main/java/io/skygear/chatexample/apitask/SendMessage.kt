79efeb8744eea4c96fe1ccd631bc59e46c48f9f6
camerash <esmond9981127@gmail.com>
2018-04-29 17:09:17 +0800

    Completed send message module

---
+++ chat_example/src/main/java/io/skygear/chatexample/apitask/SendMessage.kt
@@ -0,0 +1,68 @@
+package io.skygear.chatexample.apitask
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import io.skygear.chatexample.ApiTask
+import io.skygear.chatexample.logger.Log
+import io.skygear.plugins.chat.*
+import io.skygear.skygear.Container
+import io.skygear.skygear.Error
+
+/**
+ * Created by camerash on 4/29/18.
+ * Send
+ */
+class SendMessage: ApiTestModule {
+    
+    override fun onLoadCustomView(context: Context): View? {
+        return null
+    }
+
+    override fun onApiTest(activity: Activity, skygear: Container, chatContainer: ChatContainer, task: ApiTask, view: View) {
+        val conversationID = task.params["conversationID"]
+        val message = task.params["message"]
+        if(conversationID == null) {
+            Log.i(javaClass.simpleName, "Error: conversationID null")
+            return
+        }
+        if(message == null) {
+            Log.i(javaClass.simpleName, "Error: message null")
+            return
+        }
+
+        Log.i(javaClass.simpleName, "Fetching conversation...")
+        chatContainer.getConversation(conversationID, object: GetCallback<Conversation> {
+            override fun onSucc(obj: Conversation?) {
+                if(obj == null) {
+                    Log.i(javaClass.simpleName, "Fetch conversation failed with reason: Conversation null")
+                    return
+                }
+
+                Log.i(javaClass.simpleName, "Fetch conversation successful")
+                Log.i(javaClass.simpleName, "Sending message...")
+
+                chatContainer.sendMessage(obj, message, null, null, object: SaveCallback<Message>{
+                    override fun onSucc(msg: Message?) {
+                        if(msg == null) {
+                            Log.i(javaClass.simpleName, "Send message failed with reason: Message null")
+                            return
+                        }
+
+                        Log.i(javaClass.simpleName, "Message sent, message ID: ${msg.id}")
+                        Log.i(javaClass.simpleName, "Message ID copied to clipboard")
+                        Utils.copyToClipboard(activity, javaClass.simpleName, msg.id)
+                    }
+
+                    override fun onFail(error: Error) {
+                        Log.i(javaClass.simpleName, "Send message failed with reason: ${error.detailMessage}")
+                    }
+                })
+            }
+
+            override fun onFail(error: Error) {
+                Log.i(javaClass.simpleName, "Fetch conversation failed with reason: ${error.detailMessage}")
+            }
+        })
+    }
+}
